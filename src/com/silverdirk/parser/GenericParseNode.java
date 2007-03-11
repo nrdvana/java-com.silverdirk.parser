ec13a7c26fd54dcdae3571e464d42613fe8024fc
silverdirk <silverdirk@conserv.silverdirk.com>
2007-03-11 17:19:34 -0500

    Greatly improved the rendering of the parse tree in GenericParseNode

--- src/com/silverdirk/parser/GenericParseNode.java
+++ src/com/silverdirk/parser/GenericParseNode.java
@@ -1,6 +1,7 @@
 package com.silverdirk.parser;
 
 import java.io.PrintWriter;
+import java.util.*;
 
 /**
  * <p>Project: Dynamic LR(1) Parsing Library</p>
@@ -41,21 +42,65 @@ public class GenericParseNode {
 	}
 
 	public void displayTree(PrintWriter out) {
-		displayTree(out, "", "");
-	}
-	public void displayTree(PrintWriter out, String prefix, String connector) {
-		out.println(prefix+connector+"__"+type+"");
-		String subPrefix= prefix+" |";
-		String subConnector= "_";
-		for (int i=0; i<components.length; i++) {
-			if (i == components.length-1) {
-				subPrefix= prefix+"  ";
-				subConnector= "\\";
-			}
-			if (components[i] instanceof GenericParseNode)
-				((GenericParseNode)components[i]).displayTree(out, subPrefix, subConnector);
-			else
-				out.println(subPrefix+subConnector+"__ "+components[i]);
+		Iterator lineItr= getDisplayLines().iterator();
+		while (lineItr.hasNext()) {
+			Iterator partItr= ((LinkedList)lineItr.next()).iterator();
+			while (partItr.hasNext())
+				out.print(partItr.next());
+			out.println();
 		}
 	}
+
+	static final String spaces= "                              ";
+	static final String getSpaces(int count) {
+		if (count <= spaces.length())
+			return spaces.substring(0, count);
+		else {
+			StringBuffer sb= new StringBuffer(count);
+			while (count - sb.length() > spaces.length())
+				sb.append(spaces);
+			sb.append(getSpaces(count - sb.length()));
+			return sb.toString();
+		}
+	}
+
+	LinkedList getDisplayLines() {
+		String middle= "-"+type.toString();
+		String spacer= getSpaces(middle.length());
+		LinkedList lines= new LinkedList();
+		for (int i=0; i<components.length; i++) {
+			if (i != 0)
+				lines.add(new LinkedList());
+			if (components[i] instanceof GenericParseNode)
+				lines.addAll(((GenericParseNode)components[i]).getDisplayLines());
+			else {
+				LinkedList line= new LinkedList();
+				line.add("- "+components[i].toString());
+				lines.add(line);
+			}
+		}
+		Iterator lineItr= lines.iterator();
+		int curNode= 0, curLine= 0;
+		while (lineItr.hasNext()) {
+			LinkedList line= (LinkedList) lineItr.next();
+			boolean isNodeLine= (line.size() > 0) && (((String)line.getFirst()).charAt(0) != ' ');
+			boolean isMidLine= curLine == (lines.size()>>1);
+			if (components.length > 1) {
+				if (isNodeLine && curNode == 0)
+					line.addFirst(" ,");
+				else if (isNodeLine && curNode == components.length-1)
+					line.addFirst(" `");
+				else if (curNode > 0 && curNode < components.length)
+					line.addFirst(isMidLine? "-|":" |");
+				else
+					line.addFirst("  ");
+			}
+			line.addFirst(isMidLine? middle : spacer);
+			if (isNodeLine)
+				curNode++;
+			curLine++;
+		}
+		return lines;
+	}
+
 }
